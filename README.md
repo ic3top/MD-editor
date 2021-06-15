9959a87b310b4b8d81350d879e79b37233d84ffc
ic3top <potatai123@gmail.com>
2021-06-15 21:10:22 +0300

    fix live-DEMO link

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Markdown editor 
 [showdownjs](http://showdownjs.com/), [codemirror](https://codemirror.net/index.html) were used.  
 
-Live [DEMO](https://ic3top.github.io/Markdown-editor/dist/)  
+Live [DEMO](https://ic3top.github.io/MD-editor/dist/)  
 
 status: _in-dev_
--- dist/js/about.7b0ef7ae.js
+++ dist/js/about.7b0ef7ae.js
@@ -1,6 +1,2 @@
-(window.webpackJsonp = window.webpackJsonp || []).push([['about'], {
-  f820(n, t, a) {
-    a.r(t); const c = a('7a23'); const e = { class: 'about' }; const o = Object(c.d)('h1', null, 'This is an about page', -1); function s(n, t) { return Object(c.m)(), Object(c.c)('div', e, [o]); } const u = {}; u.render = s; t.default = u;
-  },
-}]);
-// # sourceMappingURL=about.7b0ef7ae.js.map
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23"),e={class:"about"},o=Object(c["d"])("h1",null,"This is an about page",-1);function s(n,t){return Object(c["m"])(),Object(c["c"])("div",e,[o])}const u={};u.render=s;t["default"]=u}}]);
+//# sourceMappingURL=about.7b0ef7ae.js.map
